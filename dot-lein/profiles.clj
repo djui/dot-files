ee32c69585fcfd45c2f881707a32b4cbb6be9453
Uwe Dauernheim <uwe@dauernheim.net>
2013-10-11 11:14:24 +0200

    Use egrep instead of grep

--- dot-lein/profiles.clj
+++ dot-lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[jonase/eastwood "0.0.2"]
+{:user {:plugins [[jonase/eastwood "0.0.3"]
                   [lein-kibit "0.0.8"]
                   [lein-cljsbuild "0.3.3"]
                   [lein-pprint "1.1.1"]
