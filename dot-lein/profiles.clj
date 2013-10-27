60ecf417180bcad76e7af84765db7c837ed22cb4
Uwe Dauernheim <uwe@dauernheim.net>
2013-10-27 17:37:16 +0100

    Leiningen update and new plugin

--- dot-lein/profiles.clj
+++ dot-lein/profiles.clj
@@ -2,5 +2,6 @@
                   [lein-kibit "0.0.8"]
                   [lein-cljsbuild "0.3.3"]
                   [lein-pprint "1.1.1"]
-                  [lein-try "0.3.2"]]
+                  [lein-try "0.3.2"]
+                  [lein-midje "3.1.1"]]
         :signing {:gpg-key "B87D8C4C"}}}
