551481a5ff968239fa61169494b1e2d4fe25ab2d
Uwe Dauernheim <uwe@dauernheim.net>
2014-05-24 21:34:22 +0200

    Updates

--- dot-lein/profiles.clj
+++ dot-lein/profiles.clj
@@ -1,7 +1,11 @@
-{:user {:plugins [[jonase/eastwood "0.0.3"]
+{:user {:signing {:gpg-key "B87D8C4C"}
+        :plugins [[jonase/eastwood "0.0.3"]
                   [lein-kibit "0.0.8"]
                   [lein-cljsbuild "0.3.3"]
                   [lein-pprint "1.1.1"]
                   [lein-try "0.3.2"]
                   [lein-midje "3.1.1"]]
-        :signing {:gpg-key "B87D8C4C"}}}
+        :dependencies [[clj-stacktrace "0.2.7"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)  'print-cause-trace)
+                           new  (ns-resolve (doto 'clj-stacktrace.repl require) 'pst)]
+                       (alter-var-root orig (constantly (deref new))))]}}
