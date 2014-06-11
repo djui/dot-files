8bea8c8bc55fdd660c1f3797c97b57c7e13b6b6b
Uwe Dauernheim <uwe@dauernheim.net>
2014-06-11 08:22:15 +0200

    Fix clj plugin

--- dot-lein/profiles.clj
+++ dot-lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:signing {:gpg-key "B87D8C4C"}
-        :plugins [[jonase/eastwood "0.0.3"]
+        :plugins [[jonase/eastwood "0.1.3"]
                   [lein-kibit "0.0.8"]
                   [lein-cljsbuild "0.3.3"]
                   [lein-pprint "1.1.1"]
