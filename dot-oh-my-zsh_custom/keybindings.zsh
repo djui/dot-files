ecd57cbafc7fb54b9a0fdb9326db7c093601c7a2
Uwe Dauernheim <uwe@dauernheim.net>
2014-06-17 09:13:34 +0200

    Bring back keybindings

--- dot-oh-my-zsh_custom/keybindings.zsh
+++ dot-oh-my-zsh_custom/keybindings.zsh
@@ -1,11 +1,9 @@
-#bindkey -e # emacs key bindings
-#bindkey ' ' magic-space # also do history expansion on space
+bindkey -e                   # Emacs key bindings
+bindkey ' ' magic-space      # Do history expansion also on space
 
-#bindkey "^[b" backward-char
-#bindkey  "^b" backward-word
-#bindkey "^[f" forward-char
-#bindkey  "^f" forward-word
-#bindkey "^[d" delete-char
-#bindkey  "^d" delete-word
-#bindkey "^[w" backward-delete-char
-#bindkey  "^w" backward-delete-word
+bindkey "^[b" backward-char  # Swap ctrl/alt behaviour by default
+bindkey  "^b" backward-word
+bindkey "^[f" forward-char
+bindkey  "^f" forward-word
+bindkey "^[d" delete-char
+bindkey  "^d" delete-word
