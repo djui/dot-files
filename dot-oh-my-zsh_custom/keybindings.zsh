a05f95ee84dc74f29eb04a324d5584b58a63a45e
Uwe Dauernheim <uwe@dauernheim.net>
2015-11-03 11:14:21 +0000

    Zsh: Helper keyshortcuts

--- dot-oh-my-zsh_custom/keybindings.zsh
+++ dot-oh-my-zsh_custom/keybindings.zsh
@@ -8,3 +8,18 @@ bindkey  "^f" forward-word
 bindkey "^[d" delete-char
 bindkey  "^d" delete-word
 
+run-in-background() {
+    #LBUFFER="nohup -- $LBUFFER 2> /dev/null &!";
+    LBUFFER="nohup -- $LBUFFER 2> /dev/null &";
+}
+zle -N run-in-background
+bindkey "^[B" run-in-background
+
+insert-last-typed-word() {
+    LBUFFER="$LBUFFER "
+    zle copy-prev-shell-word
+}
+
+zle -N insert-last-typed-word
+bindkey "^[>" insert-last-typed-word
+#bindkey "^[>" copy-prev-shell-word
