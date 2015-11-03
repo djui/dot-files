bda3b7750a60caa9ace5c4a468a9fe2c6168ee4a
Uwe Dauernheim <uwe@dauernheim.net>
2015-11-03 11:14:06 +0000

    Zsh: Invert Ctrl+Alt for b+f

--- dot-oh-my-zsh_custom/keybindings.zsh
+++ dot-oh-my-zsh_custom/keybindings.zsh
@@ -1,9 +1,10 @@
 bindkey -e                   # Emacs key bindings
 bindkey ' ' magic-space      # Do history expansion also on space
 
-#bindkey "^[b" backward-char  # Swap ctrl/alt behaviour by default
-#bindkey  "^b" backward-word
-#bindkey "^[f" forward-char
-#bindkey  "^f" forward-word
-#bindkey "^[d" delete-char
-#bindkey  "^d" delete-word
+bindkey "^[b" backward-char  # Swap ctrl/alt behaviour by default
+bindkey  "^b" backward-word
+bindkey "^[f" forward-char
+bindkey  "^f" forward-word
+bindkey "^[d" delete-char
+bindkey  "^d" delete-word
+
