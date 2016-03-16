9c52da5f217de8f2f3ee6f7dd8b14939f96e577a
Uwe Dauernheim <uwe@dauernheim.net>
2016-03-16 10:24:58 +0000

    Extract exports

---
+++ dot-oh-my-zsh_custom/exports.zsh
@@ -0,0 +1,33 @@
+# Man
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# Grep
+export GREP_OPTIONS='--exclude-dir=.git'
+
+# Java
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
+# Go
+export GOPATH="$HOME/dev/go"
+
+## Docker
+# Deprecated: Using dlite for now
+eval $(docker-machine env dev)
+
+# Localization
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+
+# Editor
+if [[ -n $SSH_CONNECTION ]]; then
+    export EDITOR='emacs -nw'
+else
+    export EDITOR='emacs -nw'
+fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/dsa_id"
